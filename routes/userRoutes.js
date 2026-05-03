3a0dba5455b818b2b4d757347c52c5cf191a44e7
alessandravg1122@gmail.com <alessandravg2228@gmail.com>
2026-05-02 23:34:00 -0500

    cambios para que el dashboard funcione

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getProfile, updateProfile } from '../controllers/userController.js'
+import { getProfile, updateProfile, getUsers } from '../controllers/userController.js'
 import { authenticateToken } from '../middleware/authMiddleware.js' 
 
 const router = express.Router()
 
+router.get('/', authenticateToken, getUsers);
 router.get('/profile', authenticateToken, getProfile);
 router.put('/profile/update', authenticateToken, updateProfile);
 
