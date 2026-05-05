078c6cca9780ca27e59c66126ce380111da094d4
alessandravg1122@gmail.com <alessandravg2228@gmail.com>
2026-05-04 19:31:20 -0500

    cambios para poner los filtros del vuelo

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ import usuariosRouter from './routes/usuarios.js'
 import hospedajeDetalleRoutes from './routes/hospedajeDetalle.js'
 import reservasRouter from './routes/reservas.js'
 import habitacionesRoutes from './routes/habitaciones.js'
-import vuelosRouter from './routes/vuelos.js'
-import filtrosRouter from './routes/filtros.js'
+import vuelosRouter from './routes/filtros.js'
 import errorHandler from './middleware/errorHandler.js'
 
 const app = express()
